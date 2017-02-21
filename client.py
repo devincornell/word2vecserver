ad182c622518ef9c3e363373c71bc9924cf6ae18
Devin Cornell <devinj.cornell@gmail.com>
2017-02-21 10:05:01 -0800

    Updated server and client.

--- client.py
+++ client.py
@@ -1,22 +1,31 @@
 
 import http.client
+import json
+import numpy as np
 
 class WTVClient():
-    def __init__(self, domain='localhost', port=5000):
+    def __init__(self, domain='localhost', port=5000, timeout=2):
         self.conn = http.client.HTTPConnection(domain,port)
 
     def getvector(self, word):
 
-        self.conn.request('GET', '//getword/' + str(word))
+        self.conn.request('GET', '/getvec/' + str(word))
 
         r = self.conn.getresponse()
-
-        print(r.status)
-
-        return []
+        if r.status == 200:
+            d = json.loads(r.read().decode('utf-8'))
+            vec = np.array(d['vec'])
+        else:
+            vec = None
+        return vec
 
 
 if __name__ == '__main__':
     wtv = WTVClient()
 
-    print(wtv.getvector('china'))
+    kv = wtv.getvector('king')
+    qv = wtv.getvector('queen')
+
+    print('king norm:', np.linalg.norm(kv))
+    print('queen norm:', np.linalg.norm(qv))
+    print('cosine similarity:', np.dot(kv,qv)/(np.linalg.norm(kv)*np.linalg.norm(qv)))
--- server.py
+++ server.py
@@ -12,13 +12,26 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
-@app.route('/getword/<word>')
-def getword(word):
-
+@app.route('/getvec/<word>')
+def getvec(word):
     resp = {'word':word}
     resp['vec'] = list(map(float,model[word]))
-    #print(resp['vec'])
-    # show the user profile for that user
+    resp['status'] = 'good'
+    
+    return json.dumps(resp)
+
+@app.route('/mostsimilar', methods=['GET'])
+def mostsimilar():
+    try:
+        pos = json.loads(request.args.get('positive'))
+        neg = json.loads(request.args.get('negative'))
+        topn = json.loads(request.args.get('topn'))
+    except KeyError:
+        return json.dumps({'status':'error'})
+    
+    resp['similar'] = model.most_similar(positive=pos,negative=pos,topn=topn)
+    resp['status'] = 'good'
+    
     return json.dumps(resp)
 
 # @app.route('/post/<int:post_id>')
@@ -28,8 +41,15 @@ def getword(word):
 
 if __name__ == "__main__":
     print('Loading corpus..')
-    fname = "data/GoogleNews-vectors-negative300.bin.gz"
-    model = gensim.models.Word2Vec.load_word2vec_format(fname, binary=True)  
+    fname = "data/GoogleNews-vectors-negative300.bin"
+    #model = gensim.models.Word2Vec.load_word2vec_format(fname, binary=True)
+    with open('data.json', 'r') as f:
+        model = json.load(f)
+    #print('king', model['king'])
+    #print('queen', model['queen'])
+    #print('male', model['male'])
+    #print('female', model['female'])
+
 
     print('Runnin server..')
     app.run()
