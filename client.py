eafb7198a95891e5f5a51d4b6c46ecba389dfd69
Devin Cornell <devinj.cornell@gmail.com>
2017-02-20 23:05:03 -0800

    Updated client.

--- client.py
+++ client.py
@@ -1,6 +1,22 @@
 
 import http.client
 
+class WTVClient():
+    def __init__(self, domain='localhost', port=5000):
+        self.conn = http.client.HTTPConnection(domain,port)
+
+    def getvector(self, word):
+
+        self.conn.request('GET', '//getword/' + str(word))
+
+        r = self.conn.getresponse()
+
+        print(r.status)
+
+        return []
+
+
 if __name__ == '__main__':
+    wtv = WTVClient()
 
-
+    print(wtv.getvector('china'))
