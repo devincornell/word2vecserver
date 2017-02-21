6a5f16bf6057505f1143eb6646866fa41b2b40da
Devin Cornell <devinj.cornell@gmail.com>
2017-02-21 12:13:55 -0800

    Successfully finished first round of client-server relationships (ie. getvec and mostsimilar).

--- client.py
+++ client.py
@@ -2,30 +2,59 @@
 import http.client
 import json
 import numpy as np
+from urllib.parse import quote_plus
 
 class WTVClient():
     def __init__(self, domain='localhost', port=5000, timeout=2):
         self.conn = http.client.HTTPConnection(domain,port)
 
-    def getvector(self, word):
-
-        self.conn.request('GET', '/getvec/' + str(word))
+    def getvec(self, word):
+        self.conn.request('GET', '/getvec?word=' + word)
 
         r = self.conn.getresponse()
         if r.status == 200:
             d = json.loads(r.read().decode('utf-8'))
-            vec = np.array(d['vec'])
+            try:
+                vec = d['vec']
+            except KeyError:
+                vec = None
+            vec = np.array(vec)
         else:
             vec = None
         return vec
 
 
+    def mostsimilar(self, pos=[], neg=[], topn=5):
+        headers = {'positive':pos,'negative':neg, 'topn':topn}
+        qdata = ''
+        for k,v in headers.items():
+            if str(type(v)) == "<class 'int'>":
+                v = str(v)
+            print(type(v))
+            print(v)
+            qdata += str(k) + '=' + quote_plus(json.dumps(v)) + '&'
+
+        #qdata = list(map(str,map(quote_plus, qdata)))
+        qstr = '/mostsimilar?' + qdata
+        print('Sending:', qstr)
+        self.conn.request('GET', qstr)
+        r = self.conn.getresponse()
+        if r.status == 200:
+            d = json.loads(r.read().decode('utf-8'))
+        else:
+            d = None
+        return d
+
 if __name__ == '__main__':
-    wtv = WTVClient()
+    wtv = WTVClient(domain='localhost', port=5000)
 
-    kv = wtv.getvector('king')
-    qv = wtv.getvector('queen')
+    print('Get vectors:')
+    kv = wtv.getvec('king')
+    qv = wtv.getvec('queen')
 
+    print('Do some math:')
     print('king norm:', np.linalg.norm(kv))
     print('queen norm:', np.linalg.norm(qv))
     print('cosine similarity:', np.dot(kv,qv)/(np.linalg.norm(kv)*np.linalg.norm(qv)))
+
+    print(wtv.mostsimilar(pos=['king','queen']))
--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 
 # server.py
 import gensim
-from flask import Flask
+from flask import Flask, request
 import json
+import urllib.parse
 
 
 
@@ -10,46 +11,65 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    with open('index.html') as f:
+        resp = f.read()
+    return resp
 
-@app.route('/getvec/<word>')
-def getvec(word):
-    resp = {'word':word}
-    resp['vec'] = list(map(float,model[word]))
-    resp['status'] = 'good'
+@app.route('/getvec', methods=['GET'])
+def getvec():
+    try:
+        word = request.args.get('word')
+    except KeyError:
+        return {'status':'error','problem':'Must provide a word.'}
+    
+    try:
+        vec = list(map(float,model[word]))
+    except KeyError:
+        return {'status':'error','problem':'Word not found.'}
+
+    # build response
+    resp = {'status':'good','word':word}
+    resp['vec'] = vec
     
     return json.dumps(resp)
 
 @app.route('/mostsimilar', methods=['GET'])
 def mostsimilar():
     try:
-        pos = json.loads(request.args.get('positive'))
-        neg = json.loads(request.args.get('negative'))
-        topn = json.loads(request.args.get('topn'))
+        dat = {}
+        headers = ['positive', 'negative', 'topn']
+        for h in headers:
+            dat[h] = urllib.parse.unquote(request.args.get(h))
+            print(dat[h])
+            dat[h] = json.loads(dat[h])
     except KeyError:
         return json.dumps({'status':'error'})
     
-    resp['similar'] = model.most_similar(positive=pos,negative=pos,topn=topn)
+    print(dat)
+    dat['topn'] = int(dat['topn'])
+    resp = dict()
+    resp['similar'] = model.most_similar(positive=dat['positive'],negative=dat['negative'],topn=dat['topn'])
     resp['status'] = 'good'
     
     return json.dumps(resp)
 
-# @app.route('/post/<int:post_id>')
-# def show_post(post_id):
-#     # show the post with the given id, the id is an integer
-#     return 'Post %d' % post_id
-
-if __name__ == "__main__":
-    print('Loading corpus..')
+def runserver():
+    realmodel = True
+    domain = 'localhost'
+    port = 5000
     fname = "data/GoogleNews-vectors-negative300.bin"
-    #model = gensim.models.Word2Vec.load_word2vec_format(fname, binary=True)
-    with open('data.json', 'r') as f:
-        model = json.load(f)
-    #print('king', model['king'])
-    #print('queen', model['queen'])
-    #print('male', model['male'])
-    #print('female', model['female'])
+    
+    global model
+    if realmodel:
+        print('Loading corpus..')
+        model = gensim.models.Word2Vec.load_word2vec_format(fname, binary=True)
 
+    else:
+        with open('data.json', 'r') as f:
+            model = json.load(f)
 
     print('Runnin server..')
     app.run()
+
+if __name__ == "__main__":
+    runserver()
